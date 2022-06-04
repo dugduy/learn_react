305f3476fb95b4be365a415b2a0b4a083167d7ff
dug duy <dugduy01@gmail.com>
2022-06-04 20:03:44 +0700

    uipdate a.js

--- a.js
+++ a.js
@@ -1,2 +1,10 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, world!</h1>);
+// root.render(<h1>Hello, world!</h1>);
+function tick(){
+    const ele=<div>
+        <h1>Hello World!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+    root.render(ele)
+}
+setInterval(tick,100)
